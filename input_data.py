f330ff530d2e4aed14e657cac88c65b3c0f0e67c
daninnuzan <daninnuzan@apps.ipb.ac.id>
2026-09-18 18:05:57 +0700

    Update input_data.py

--- input_data.py
+++ input_data.py
@@ -10,7 +10,7 @@ Original file is located at
 # input_data.py
 data_mahasiswa = []
 
-def tambah_mahasiswa(nama, umur, jurusan):
+def tambah_mahasiswa(nama, umur, mint):
     mahasiswa = {
         "nama": nama,
         "umur": umur,
